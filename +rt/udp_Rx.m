7b87ba415debd00c847dcfdcdc99c8776d4b389f
qwe14789cn <qwe14789cn@gmail.com>
2019-09-29 14:28:12 +0800

    Update udp_Rx.m

--- +rt/udp_Rx.m
+++ +rt/udp_Rx.m
@@ -4,15 +4,13 @@ udpRx.LocalIPPort = port;
 udpRx.ReceiveBufferSize = 65536;
 udpRx.MaximumMessageLength = package_size(1);
 TOTAL = package_size(2);
-Rx = [];
-Received = uint8(zeros(package_size(1)));
-idx = 1;
+Received = uint8(zeros(package_size(1),package_size(2)));
 disp('waiting...')
 while TOTAL
     Rx = udpRx();
     if ~isempty(Rx)
         Received(:,package_size(2)-TOTAL+1) = Rx;
-        TOTAL = TOTAL -1;
+        TOTAL = TOTAL - 1;
     end
 end
 disp('finished')
