2e9353d1e32e21d6203e719228395988d17af851
qwe14789cn <qwe14789cn@gmail.com>
2020-05-27 11:14:48 +0800

    修改坐标旋转矩阵的命名方式

--- +ct/rotate_xd.m
+++ +ct/rotate_xd.m
@@ -1,6 +1,8 @@
 %--------------------------------------------------------------------------
 %   绕X方向的旋转矩阵
+%	example：
+%	rotate_xd(30)*[x;y;z]
 %--------------------------------------------------------------------------
-function yaw = X_rotation_degree(theta)
+function yaw = rotate_xd(theta)
 yaw = [1 0 0;0 cosd(theta) -sind(theta);0 sind(theta) cosd(theta)];
 end
