c9e14dd9d1d1242c601f61d1d227916d64213209
qwe14789cn <qwe14789cn@gmail.com>
2020-07-23 10:25:54 +0800

    Update write_bin.m

--- +rt/write_bin.m
+++ +rt/write_bin.m
@@ -1,3 +1,6 @@
+% example:
+% write_bin(download_data,'config.bin','single')
+
 function [output] = write_bin(data_name,file_name,file_type)
 f = fopen(file_name,'w');
 output = fwrite(f,data_name,file_type);
