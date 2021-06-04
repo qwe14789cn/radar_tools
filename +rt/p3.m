b6a73758da7680b2cbf34757ac48bcb18652f3bc
qwe14789cn <qwe14789cn@gmail.com>
2021-06-04 15:42:01 +0800

    Update p3.m
    新增三维画图的模板，可以标记格式

--- +rt/p3.m
+++ +rt/p3.m
@@ -4,6 +4,11 @@
 %   example
 %   p3(sig)
 %--------------------------------------------------------------------------
-function p3(signal)
-plot3(1:size(signal,1),real(signal),imag(signal));grid on
-xlabel('点数');ylabel('实部');zlabel('虚部')
+function p3(signal,mark)
+if nargin==2
+    plot3(1:size(signal,1),real(signal),imag(signal),mark);grid on
+    xlabel('点数');ylabel('实部');zlabel('虚部')
+elseif nargin ==1
+    plot3(1:size(signal,1),real(signal),imag(signal));grid on
+    xlabel('点数');ylabel('实部');zlabel('虚部')
+end
